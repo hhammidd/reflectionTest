0f5865495610518b3224d0e75bca7655c3d9721f
Ham Shafi <hamid.shafikani@gmail.com>
2018-11-14 09:47:14 +0100

    sw

--- src/com/java/reflection/MainReflection.java
+++ src/com/java/reflection/MainReflection.java
@@ -1,13 +1,13 @@
 package com.java.reflection;
 
 import com.java.reflection.model.Users;
-import com.sun.xml.internal.ws.util.StringUtils;
 
-import javax.jws.soap.SOAPBinding;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainReflection {
 
@@ -17,7 +17,6 @@ public class MainReflection {
         Users users = new Users();
         users.setUserId(1);
         users.setUserName("hamid");
-
 
         //Question 1
         Object objNew = copy(users);
@@ -31,106 +30,69 @@ public class MainReflection {
         Class clazzToObj = companyClass.getClass();
         String jsonObj = "{ \"userId\": 5, \"userName\": \"hamid\" }";
 
-        System.out.println(jsonObj);
         Object stringToObject = toObj(jsonObj,clazzToObj);
+        System.out.println(stringToObject);
     }
 
-    private static <T> T toObj(String jsonObj, Class<T> clazzToObj) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+    /**
+     * input the JSOn and Output the Object
+     * @param jsonObj
+     * @param clazzToObj
+     * @param <T>
+     * @return
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     */
+    private static Object toObj(String jsonObj, Class clazzToObj) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Object clazzObj = clazzToObj.newInstance();
 
-        T objFromString = null;
         Method[] m = clazzToObj.getDeclaredMethods();
-
-        List<Object> fieldNameList = new ArrayList<>();
-        List<Object> argFieldNewList = new ArrayList<>();
-        Map<String, Object> fieldInfoMap = new HashMap<>();
-
-        List<Map> objectInfo = new ArrayList<>();
-
+        Map<String, Object> objectInfo = new HashMap<>();
         objectInfo = parseJson(jsonObj);
-        fieldInfoMap = objectInfo.get(0);
-
-
-        System.out.println("start");
-
-        //fieldInfoMap = objectInfo.stream().map(x->x.keySet());
-        //Map<String, Object> resultField = objectInfo.stream().collect(Collectors.toMap(s->(List<String>) s.get("key"), s-> (List<Object>) s.get()));
-        //System.out.println(resultField);
-
-
-        System.out.println(argFieldNewList);
-        System.out.println(fieldNameList);
-        Object argFieldNew = new Object();
-
 
         for (Method e : m) {
             int nOfArgs = e.getParameterCount();
             String mName = e.getName();
-
-
             if (mName.startsWith("set") && Character.isUpperCase(mName.charAt(3))) {
-                // key is setUserName val is : hamid
-                //String getValOfSpecialKey = objectInfo.stream().map();
-                //String fieldNameOfMname = objectInfo.stream().map(x->x.keySet().contains(mName.substring(3)))
+
                 String fieldNameWithUpper = mName.substring(3);
                 String fieldName = fieldNameWithUpper.toLowerCase().charAt(0) + fieldNameWithUpper.substring(1);
                 System.out.println(fieldName);
-                Object argFielFromMap = new Object();
 
-                argFielFromMap =  objectInfo.stream().filter(entry-> entry.containsKey(fieldName));
-                argFielFromMap =  objectInfo.stream().filter(entry-> entry.containsKey(fieldName))
-                ;
-                System.out.println(argFielFromMap);
+                // take the arg
+                Object argFieldNew =  objectInfo.entrySet().stream().filter(eKey->eKey.getKey().equals("userId")).findFirst()
+                        .get().getValue();
 
-                Class<?>[] paramTypeNew = e.getParameterTypes();
-                Method methodSetter = clazzToObj.getDeclaredMethod(mName,paramTypeNew[0]);
-
-                methodSetter.invoke(clazzObj, argFieldNew);
+                System.out.println(e.getName());
+                Method methodSetter = clazzToObj.getDeclaredMethod(e.getName(), e.getReturnType());
+                e.invoke(clazzObj, argFieldNew);
             }
         }
-        return (T) clazzObj;
+        return clazzObj;
     }
 
-    private static List<Map> parseJson(String jsonObj) {
 
-        // { "A" :a, "B" : "b"} start test
-        //String jsonStr = "{ \"Aaad\" :a, \"B\" : \"b\" , \"C\" : \"c\"}";
-        // split ,
-        //TODO find all values here from JSON
-        List<Map> findValue = fineValue(jsonObj);
-        return findValue;
-    }
-
-    private static List<Map> fineValue(String jsonObj) {
+    private static Map<String, Object> parseJson(String jsonObj) {
 
         jsonObj = jsonObj.substring(jsonObj.indexOf("{") +1, jsonObj.indexOf("}")).trim();
+
 
         String[] splitVir = jsonObj.split(",");
 
         String fieldNameRow = "";
-        Object valueRow = new Object();
-        List<String> fieldList = new ArrayList<>();
-        List<Object> valueList = new ArrayList<>();
-
         Map<String, Object> infoJson = new HashMap<>();
-        List<Map> listInfoJsonMap = new ArrayList<>();
 
         for (int i = 0; i < splitVir.length ; i++){
-            System.out.println(splitVir[i]);
             Object[] splitSubVir = splitVir[i].split(":");
             fieldNameRow = (String) splitSubVir[0];
             //System.out.println("before: " + fieldNameRow);
-            String fieldName = fieldNameRow.replace("\"", "");
-            fieldName.trim();
-            fieldName.replaceAll("\\s+","");
+            String fieldName = fieldNameRow.replace("\"", "").trim().replaceAll("\\s+","");
 
-            infoJson.put(fieldName,splitSubVir[1]);
-
-            fieldList.add(fieldName);
-            valueList.add(splitSubVir[1]);
+            infoJson.put(fieldName,splitSubVir[1].toString().trim());
         }
-        listInfoJsonMap.add(infoJson);
-        return listInfoJsonMap;
+        return  infoJson;
     }
 
     /**
