098855160a7b832df379673f61558f66602ab1d2
Ham Shafi <hamid.shafikani@gmail.com>
2018-11-14 10:38:28 +0100

    sw

--- src/com/java/reflection/MainReflection.java
+++ src/com/java/reflection/MainReflection.java
@@ -4,9 +4,7 @@ import com.java.reflection.model.Users;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainReflection {
@@ -30,12 +28,13 @@ public class MainReflection {
         Class clazzToObj = companyClass.getClass();
         String jsonObj = "{ \"userId\": 5, \"userName\": \"hamid\" }";
 
-        Object stringToObject = toObj(jsonObj,clazzToObj);
+        Object stringToObject = toObj(jsonObj, clazzToObj);
         System.out.println(stringToObject);
     }
 
     /**
      * input the JSOn and Output the Object
+     *
      * @param jsonObj
      * @param clazzToObj
      * @param <T>
@@ -48,26 +47,26 @@ public class MainReflection {
     private static Object toObj(String jsonObj, Class clazzToObj) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Object clazzObj = clazzToObj.newInstance();
 
-        Method[] m = clazzToObj.getDeclaredMethods();
         Map<String, Object> objectInfo = new HashMap<>();
         objectInfo = parseJson(jsonObj);
 
-        for (Method e : m) {
-            int nOfArgs = e.getParameterCount();
-            String mName = e.getName();
+        for (Method method : clazzToObj.getDeclaredMethods()) {
+            int nOfArgs = method.getParameterCount();
+            String mName = method.getName();
             if (mName.startsWith("set") && Character.isUpperCase(mName.charAt(3))) {
 
                 String fieldNameWithUpper = mName.substring(3);
                 String fieldName = fieldNameWithUpper.toLowerCase().charAt(0) + fieldNameWithUpper.substring(1);
                 System.out.println(fieldName);
 
-                // take the arg
-                Object argFieldNew =  objectInfo.entrySet().stream().filter(eKey->eKey.getKey().equals("userId")).findFirst()
+                // take the arg TODO delete userId
+                Object argFieldNew = objectInfo.entrySet().stream().filter(eKey -> eKey.getKey().equals("userId")).findFirst()
                         .get().getValue();
 
-                System.out.println(e.getName());
-                Method methodSetter = clazzToObj.getDeclaredMethod(e.getName(), e.getReturnType());
-                e.invoke(clazzObj, argFieldNew);
+                // you have to cast Object to the value is comming
+                System.out.println(method.getName());
+                Method methodSetter = clazzToObj.getDeclaredMethod(method.getName(), method.getParameterTypes());
+                methodSetter.invoke(clazzObj, 5);
             }
         }
         return clazzObj;
@@ -76,7 +75,7 @@ public class MainReflection {
 
     private static Map<String, Object> parseJson(String jsonObj) {
 
-        jsonObj = jsonObj.substring(jsonObj.indexOf("{") +1, jsonObj.indexOf("}")).trim();
+        jsonObj = jsonObj.substring(jsonObj.indexOf("{") + 1, jsonObj.indexOf("}")).trim();
 
 
         String[] splitVir = jsonObj.split(",");
@@ -84,19 +83,20 @@ public class MainReflection {
         String fieldNameRow = "";
         Map<String, Object> infoJson = new HashMap<>();
 
-        for (int i = 0; i < splitVir.length ; i++){
+        for (int i = 0; i < splitVir.length; i++) {
             Object[] splitSubVir = splitVir[i].split(":");
             fieldNameRow = (String) splitSubVir[0];
             //System.out.println("before: " + fieldNameRow);
-            String fieldName = fieldNameRow.replace("\"", "").trim().replaceAll("\\s+","");
+            String fieldName = fieldNameRow.replace("\"", "").trim().replaceAll("\\s+", "");
 
-            infoJson.put(fieldName,splitSubVir[1].toString().trim());
+            infoJson.put(fieldName, splitSubVir[1].toString().trim());
         }
-        return  infoJson;
+        return infoJson;
     }
 
     /**
-     *  Q3
+     * Q3
+     *
      * @param obj
      * @return Json String of the Object
      * @throws InvocationTargetException
@@ -105,13 +105,12 @@ public class MainReflection {
      */
     private static String toJson(Object obj) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         Class clazz = obj.getClass();
-        Method[] m = clazz.getDeclaredMethods();
         String toJsoned = "{\n";
         String toJsonedFor = "";
 
-        for (Method e : m) {
-            int nOfArgs = e.getParameterCount();
-            String mName = e.getName();
+        for (Method method : clazz.getDeclaredMethods()) {
+            int nOfArgs = method.getParameterCount();
+            String mName = method.getName();
             if (mName.startsWith("get") && nOfArgs == 0 && Character.isUpperCase(mName.charAt(3))) {
                 String fieldNameWithUpper = mName.substring(3);
                 String fieldName = fieldNameWithUpper.toLowerCase().charAt(0) + fieldNameWithUpper.substring(1);
@@ -144,25 +143,21 @@ public class MainReflection {
             NoSuchMethodException, InstantiationException {
 
         Class clazz = obj.getClass();
-        Object clazzObj = obj.getClass().newInstance();
+        Object newObj = clazz.newInstance();
 
-        Method[] m = clazz.getDeclaredMethods();
-
-        String mSetter = "";
-        Object argFieldNew = new Object();
-        for (Method e : m) {
-            int nOfArgs = e.getParameterCount();
-            String mName = e.getName();
+        Object value = new Object();
+        for (Method method : clazz.getDeclaredMethods()) {
+            int nOfArgs = method.getParameterCount();
+            String mName = method.getName();
 
             if (mName.startsWith("get") && nOfArgs == 0 && Character.isUpperCase(mName.charAt(3))) {
-                Method methodCall = clazz.getMethod(mName);
-                argFieldNew = methodCall.invoke(obj);
+                value = method.invoke(obj);
 
-                mSetter = "s" + mName.substring(1);
-                Method methodSetter = clazz.getDeclaredMethod(mSetter, e.getReturnType());
-                methodSetter.invoke(clazzObj, argFieldNew);
+                String mSetter = "s" + mName.substring(1);
+                Method methodSetter = clazz.getDeclaredMethod(mSetter, method.getReturnType());
+                methodSetter.invoke(newObj, value);
             }
         }
-        return clazzObj;
+        return newObj;
     }
 }
